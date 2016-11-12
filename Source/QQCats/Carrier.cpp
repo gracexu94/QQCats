087f8f749ec44823cf2c5ff6ed9aa1510310a4db
Kangning Li <likangning93@gmail.com>
2016-11-12 17:42:15 -0500

    carrier seems to be working!

--- Source/QQCats/Carrier.cpp
+++ Source/QQCats/Carrier.cpp
@@ -10,7 +10,15 @@ ACarrier::ACarrier()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	OnActorHit.AddDynamic(this, &ACarrier::onHit); // used to call OnHit
+
+	AsteroidBoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("RootComponent"));
+	RootComponent = AsteroidBoxComponent;
+	AsteroidBoxComponent->InitBoxExtent(FVector(80.0f, 100.0f, 70.0f));
+	AsteroidBoxComponent->SetCollisionProfileName(TEXT("BlockAllDynamic"));
+	AsteroidBoxComponent->SetSimulatePhysics(true);
+	AsteroidBoxComponent->SetEnableGravity(false);
+	AsteroidBoxComponent->SetNotifyRigidBodyCollision(true);
+	OnActorHit.AddDynamic(this, &ACarrier::onHit);
 }
 
 // Called when the game starts or when spawned
@@ -30,11 +38,12 @@ void ACarrier::onHit(AActor* SelfActor, class AActor* OtherActor, FVector Normal
 	const FHitResult& Hit) {
 
 
-	UE_LOG(LogTemp, Warning, TEXT("something hit the carrier"));
+	UE_LOG(LogTemp, Warning, TEXT("something hit the carrier, is %s"), *OtherActor->GetName());
 
 
 	if (OtherActor && (OtherActor != this) && OtherActor->IsA(ACat::StaticClass()))
 	{
+		UE_LOG(LogTemp, Warning, TEXT("cat"));
 		Destroy();
 		OtherActor->Destroy();
 		UWorld* const World = GetWorld();
